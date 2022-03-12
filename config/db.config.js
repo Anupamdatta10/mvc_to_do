52a864fdc304d0fb2eca88f91f9a61e55e7c6e53
Anupamdatta10 <anupamdatta10@gmail.com>
2022-03-12 10:37:37 +0530

    config change

--- config/db.config.js
+++ config/db.config.js
@@ -4,10 +4,11 @@ const mysql = require('mysql');
 
 //local mysql db connection
 const dbConn = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  password : '',
-  database : 'test'
+  host     : 'sql6.freemysqlhosting.net',
+  user     : 'sql6477712',
+  password : 'dubCfvK2J2',
+  database : 'sql6477712',
+  port     : '3306'
 });
 dbConn.connect(function(err) {
   if (err) throw err;
